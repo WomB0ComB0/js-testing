a765b4662e64f21d19451f8b4d614f0e5e6b7656
Mike Odnis <mike@mikeodnis.dev>
2025-06-05 23:18:50 -0400

    feat: List collections and documents in MongoDB
    - Added code to list all collections in the database.
    - Added code to list all documents in the 'test' collection.

--- mongo.ts
+++ mongo.ts
@@ -1,13 +1,30 @@
 import mongoose from 'mongoose';
 
 (async () => {
+  const testSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+  });
+  const Test = mongoose.model('Test', testSchema);
+
   try {
     await mongoose.connect('mongodb://127.0.0.1:27017/test', {
       user: 'test',
       pass: 'test',
-      authSource: 'admin',
+      authSource: 'admin'
     });
     console.log('Connected to MongoDB');
+    const db = mongoose.connection.db;
+
+    if (db) {
+      const collections = await db.listCollections().toArray().then(collections => collections.map(collection => collection.name));
+      console.log(collections);
+    }
+    const collection = db?.collection('test');
+    if (collection) {
+      const documents = await collection.find({}).toArray().then(documents => documents.map(document => document.name));
+      console.log(documents);
+    }
   } catch (error) {
     console.error('Connection error:', error);
   } finally {
